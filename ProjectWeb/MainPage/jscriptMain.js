ba7bb973eb182c1f156b437e501df7e885460410
DarkhanAmanzhol <darhan25amanzhol@gmail.com>
2019-11-21 23:42:14 +0600

    Add files via upload

---
+++ ProjectWeb/MainPage/jscriptMain.js
@@ -0,0 +1,23 @@
+let scroll = window.requestAnimationFrame;
+let elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+function loop() {
+
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('is-visible');
+    } else {
+      element.classList.remove('is-visible');
+    }
+  });
+
+  scroll(loop);
+}
+loop();
+function isElementInViewport(el) {
+  let rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) && 
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)));
+}
