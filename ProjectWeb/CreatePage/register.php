2c69f18a249695ac02224a35c28871128c5f1700
DarkhanAmanzhol <darhan25amanzhol@gmail.com>
2019-11-23 16:30:12 +0600

    Create register.php

---
+++ ProjectWeb/CreatePage/register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+<div class="container">
+	<div class="">
+		<div>
+			<h1>Register Now / <a href="">Login Now</a></h1>
+			<form method="POST">
+				<input type="text" name="name" placeholder="Enter Your Name">
+				<input type="email" name="email" placeholder="Enter Your Email">
+				<input type="password" name="pass" placeholder="*******">
+				<input type="submit" name="submit">
+				<input type="reset" name="reset">
+			</form>
+		</div>
+	</div>
+</div>
+</body>
+</html>
