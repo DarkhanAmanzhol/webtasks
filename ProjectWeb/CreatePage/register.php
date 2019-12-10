5b3856cdc6d2ca644fc95b1f625682e7d12e42ad
DarkhanAmanzhol <darhan25amanzhol@gmail.com>
2019-12-10 16:43:58 +0600

    upgraded register.php

--- ProjectWeb/CreatePage/register.php
+++ ProjectWeb/CreatePage/register.php
@@ -2,22 +2,55 @@
 <html>
 <head>
 	<title>Login</title>
+		<link href="cssCreate.css" rel="stylesheet" type="text/css">
 </head>
-<body>
+<body style="overflow-x: hidden;">
+	<div class="fixedBoard">
+          <ul class="nav">
+            <li><a href="#" class="contact">News</a></li>
+            <li><a href="#" class="about">About</a></li>
+            <li><a href="../MainPage/Main Page.html" class="home">Home</a></li>
+            <li><div class="logophoto"><img src="laptop-64.png"></div></li>
+			<li><div class="logoword">Laptop.com</div></li>
+			<li><div class="phone"><img src="phone.png">+7-747-111-25-04</li>
+          </ul>
+	</div>
 <div class="container">
-	<div class="">
-		<div>
-			<h1>Register Now / <a href="">Login Now</a></h1>
-			<form method="POST">
-				<input type="text" name="name" placeholder="Enter Your Name">
-				<input type="email" name="email" placeholder="Enter Your Email">
-				<input type="password" name="pass" placeholder="*******">
-				<input type="submit" name="submit">
-				<input type="reset" name="reset">
-			</form>
-		</div>
+	<h1>Register Now / <a href="login.php">Login Now</a></h1>
+	<div class="toPlace">
+		<form method="POST">
+			<input type="text" name="name" placeholder="Enter Your Name" class="writer"><br>
+			<input type="email" name="email" placeholder="Enter Your Email" class="writer"><br>
+			<input type="password" name="pass" placeholder="*******" class="writer"><br>
+			<input type="submit" name="submit" value="Submit" class="btn">
+			<input type="reset" name="reset" value="Reset" class="btn">
+		</form>
 	</div>
 </div>
+	<footer>
+            <div class="down">
+            	<img class="centerSmile" src="smiling.png">
+                <div class="col12">
+                    <p class="bot">&copy; Designed by Darkhan</p>
+                    <div class="space20"></div>
+                </div>
+                <div class="col13">
+                	<p class="left">Do you know about our social media?</p>
+                	<p class="left_Desc">We are small company which wants to sell and buy goods efficiently!</p>
+                	<img class="leftImage" src="amazon-logo.png">
+                	<img class="leftImage" src="apple-logo.png">
+                	<img class="leftImage" src="facebook-logo.png">
+                </div>
+                <div class="col14">
+                	<p class="right">About our company</p>
+                	<p class="right_Desc des1">About laptops</p>
+                	<p class="right_Desc des2">Care of laptops</p>
+                	<p class="right_Desc des3">Carier in the Laptop.com</p>
+                	<p class="right_Desc des4">Contact</p>
+                	<p class="right_Desc des5">Our shops</p>
+                </div>
+            </div>
+    </footer>
 </body>
 </html>
 
