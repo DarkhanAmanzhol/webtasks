0bb0a3c3d383fefc05a8c5f61cb2ffd1b7fc508d
DarkhanAmanzhol <darhan25amanzhol@gmail.com>
2019-12-13 15:07:36 +0600

    Update register.php

--- ProjectWeb/CreatePage/register.php
+++ ProjectWeb/CreatePage/register.php
@@ -2,21 +2,15 @@
 <html>
 <head>
 	<title>Login</title>
-		<link href="cssCreate.css" rel="stylesheet" type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="cssCreate.css" rel="stylesheet" type="text/css">
 </head>
 <body style="overflow-x: hidden;">
-	<div class="fixedBoard">
-          <ul class="nav">
-            <li><a href="#" class="contact">News</a></li>
-            <li><a href="#" class="about">About</a></li>
-            <li><a href="../MainPage/Main Page.html" class="home">Home</a></li>
-            <li><div class="logophoto"><img src="laptop-64.png"></div></li>
-			<li><div class="logoword">Laptop.com</div></li>
-			<li><div class="phone"><img src="phone.png">+7-747-111-25-04</li>
-          </ul>
-	</div>
+	<header>
+		<a href="../MainPage/Main Page.html">Go Home</a>
+	</header>
 <div class="container">
-	<h1>Register Now / <a href="login.php">Login Now</a></h1>
+	<h2>Register Now / <a href="login.php">Login Now</a></h2>
 	<div class="toPlace">
 		<form method="POST">
 			<input type="text" name="name" placeholder="Enter Your Name" class="writer"><br>
@@ -27,33 +21,9 @@
 		</form>
 	</div>
 </div>
-	<footer>
-            <div class="down">
-            	<img class="centerSmile" src="smiling.png">
-                <div class="col12">
-                    <p class="bot">&copy; Designed by Darkhan</p>
-                    <div class="space20"></div>
-                </div>
-                <div class="col13">
-                	<p class="left">Do you know about our social media?</p>
-                	<p class="left_Desc">We are small company which wants to sell and buy goods efficiently!</p>
-                	<img class="leftImage" src="amazon-logo.png">
-                	<img class="leftImage" src="apple-logo.png">
-                	<img class="leftImage" src="facebook-logo.png">
-                </div>
-                <div class="col14">
-                	<p class="right">About our company</p>
-                	<p class="right_Desc des1">About laptops</p>
-                	<p class="right_Desc des2">Care of laptops</p>
-                	<p class="right_Desc des3">Carier in the Laptop.com</p>
-                	<p class="right_Desc des4">Contact</p>
-                	<p class="right_Desc des5">Our shops</p>
-                </div>
-            </div>
-    </footer>
+    <footer><p class="bot">&copy; Designed by Darkhan</p></footer>
 </body>
 </html>
-
 <?php
 $conn = mysqli_connect("localhost","root","","user") or die("Connection Error");
 ?>
