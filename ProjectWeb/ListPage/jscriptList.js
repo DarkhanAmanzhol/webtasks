2de6df447d1b9ccbd679bac4dc7366f1d63b37ab
DarkhanAmanzhol <darhan25amanzhol@gmail.com>
2019-12-22 17:07:00 +0600

    Update jscriptList.js

--- ProjectWeb/ListPage/jscriptList.js
+++ ProjectWeb/ListPage/jscriptList.js
@@ -1,124 +1,15 @@
-var i1_1 = 0;
-var i1_2 = 0;
-var i1_3 = 0;
-var i1_4 = 0;
-function clickRight(row){
-	var i = row.parentNode.parentNode.rowIndex;
-	let a = "mackbookpro19.jpg";
-	let b = "dellxps13.jpg";
-	let c = "acerswitch3.jpg";
-	let d = "surfacepro.jpg";
-	let a1 = "mac1.jpg";
-	let b1 = "dell1.jpg";
-	let c1 = "acer1.jpg";
-	let d1 = "surface1.jpg";
-	if(i==0){
-		i1_1 = i1_1 + 1;
-		if(i1_1%2!=0){
-			document.getElementById("good0").src = a1;
-		}
-		else{
-			document.getElementById("good0").src = a;
-		}
-	}
-	if(i==1){
-		i1_2 = i1_2 + 1;
-		if(i1_2%2!=0){
-			document.getElementById("good1").src = b1;
-		}
-		else{
-			document.getElementById("good1").src = b;
-		}
-	}
-	if(i==2){
-		i1_3 = i1_3 + 1;
-		if(i1_3%2!=0){
-			document.getElementById("good2").src = c1;
-		}
-		else{
-			document.getElementById("good2").src = c;
-		}
-	}
-	if(i==3){
-		i1_4 = i1_4 + 1;
-		if(i1_4%2!=0){
-			document.getElementById("good3").src = d1;
-		}
-		else{
-			document.getElementById("good3").src = d;
-		}
-	}
-}
-var i2_1 = 0;
-var i2_2 = 0;
-var i2_3 = 0;
-var i2_4 = 0;
-function clickLeft(row){
-	var i = row.parentNode.parentNode.rowIndex;
-	let a = "mackbookpro19.jpg";
-	let b = "dellxps13.jpg";
-	let c = "acerswitch.jpg";
-	let d = "surfacepro.jpg";
-	let a1 = "mac1.jpg";
-	let b1 = "dell1.jpg";
-	let c1 = "acer1.jpg";
-	let d1 = "surface1.jpg";
-	if(i==0){
-		i2_1 = i2_1 + 1;
-		if(i2_1%2==0){
-			document.getElementById("good0").src = a;
-		}
-		else{
-			document.getElementById("good0").src = a1;
-		}
-	}
-	if(i==1){
-		i2_2 = i2_2 + 1;
-		if(i2_2%2!=0){
-			document.getElementById("good1").src = b1;
-		}
-		else{
-			document.getElementById("good1").src = b;
-		}
-	}
-	if(i==2){
-		i2_3 = i2_3 + 1;
-		if(i2_3%2!=0){
-			document.getElementById("good2").src = c1;
-		}
-		else{
-			document.getElementById("good2").src = c;
-		}
-	}
-	if(i==3){
-		i2_4 = i2_4 + 1;
-		if(i2_4%2!=0){
-			document.getElementById("good3").src = d1;
-		}
-		else{
-			document.getElementById("good3").src = d;
-		}
-	}
+var array = [];
+function adder(product){
+	console.log(product);
+	array.push(product);
+	console.log(array); 
 }
 function showCart(){
-	let a = document.getElementById("cart").style.display;
-	console.log(a);
+	document.getElementById("cartingInvisible").style.display = 'block';
+	document.getElementById("allGoods").innerHTML = array;
+	console.log(array); 
+	document.body.style.backgroundColor = "rgba(10, 10, 10, 0.4)";
 }
-
-
-
-var tabButtons = document.querySelectorAll("#buttonContainer button");
-var tabPanels = document.querySelectorAll("#tabPanel");
-
-function showPanel(panelIndex) {
-	tabButtons.forEach(function(node){
-		node.style.backgroundColor="";
-		node.style.color="";
-	});
-	tabButtons[panelIndex].style.backgroundColor='gray';
-	tabButtons[panelIndex].style.color="blueviolet";
-	tabPanels.forEach(function(node){
-		node.style.display="none";
-	});
-	tabPanels[panelIndex].style.display="block";
+function okBack(){
+	document.getElementById("cartingInvisible").style.display = 'none';
 }
